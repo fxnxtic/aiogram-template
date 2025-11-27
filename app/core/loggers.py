c33fa51fcb8550dcae9be1c1fa6cc06bedfaa417
fxnxtic <thedinalt@gmail.com>
2025-11-27 22:50:41 +0400

    refactor: remove doubled processors in init_logging & remove unused code

--- app/core/loggers.py
+++ app/core/loggers.py
@@ -1,6 +1,5 @@
 import logging
 import sys
-import traceback
 
 import structlog
 
@@ -11,11 +10,10 @@ class MuteDropper(logging.Filter):
         self.names_to_mute = set(names_to_mute)
 
     def filter(self, record: logging.LogRecord) -> bool:
-        # Возвращаем False → log будет отброшен
         return record.name not in self.names_to_mute
-    
 
-def init_logging(*mute_loggers: str, debug: bool = False) -> None:
+
+def init_logging(mute_loggers: list[str] | None = None, debug: bool = False) -> None:
     """
     Initializes the logging system.
 
@@ -23,11 +21,14 @@ def init_logging(*mute_loggers: str, debug: bool = False) -> None:
         *mute_loggers (str): The names of loggers to mute.
         debug (bool, optional): Whether to enable debug logging. Defaults to False.
     """
+    mute_loggers = [] if mute_loggers is None else mute_loggers
+
     logging.captureWarnings(True)
+
     logging.basicConfig(
-        format="%(message)s",
-        stream=sys.stdout,
         level=logging.DEBUG if debug else logging.INFO,
+        stream=sys.stdout,
+        format="%(message)s",
     )
 
     renderer = (
@@ -40,44 +41,27 @@ def init_logging(*mute_loggers: str, debug: bool = False) -> None:
         processors=[
             structlog.processors.TimeStamper(fmt="iso"),
             structlog.stdlib.add_log_level,
-            structlog.processors.StackInfoRenderer(),
             structlog.processors.format_exc_info,
             renderer,
         ],
-        context_class=dict,
         logger_factory=structlog.stdlib.LoggerFactory(),
         wrapper_class=structlog.stdlib.BoundLogger,
         cache_logger_on_first_use=True,
     )
 
-    formatter = structlog.stdlib.ProcessorFormatter(
-        processor=renderer,
-        foreign_pre_chain=[
-            structlog.stdlib.add_logger_name,
-            structlog.stdlib.add_log_level,
-            structlog.processors.TimeStamper(fmt="iso"),
-        ]
-    )
-
     handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(formatter)
     handler.addFilter(MuteDropper(mute_loggers))
 
-    root_logger = logging.getLogger()
-    root_logger.handlers = [handler]
+    handler.setFormatter(
+        structlog.stdlib.ProcessorFormatter(
+            processor=renderer,
+            foreign_pre_chain=[
+                structlog.stdlib.add_log_level,
+                structlog.processors.TimeStamper(fmt="iso"),
+            ],
+        )
+    )
 
-
-def log_errors(raise_error: bool = False):
-    def decorator(func):
-        async def wrapper(*args, **kwargs):
-            try:
-                return await func(*args, **kwargs)
-            except Exception as e:
-                logger = structlog.get_logger(__name__)
-                logger.error(traceback.format_exc())
-                if raise_error:
-                    raise e
-
-        return wrapper
-
-    return decorator
+    root = logging.getLogger()
+    root.handlers.clear()
+    root.addHandler(handler)
